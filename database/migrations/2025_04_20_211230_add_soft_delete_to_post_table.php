46031fb42bbbbb2263c86082547bc1d418da9d2c
hatimbelghiti <belghitihatim00@gmail.com>
2025-04-21 10:06:56 +0100

    adding chat table and migration to be able to use laravel broadcast

--- database/migrations/2025_04_20_211230_add_soft_delete_to_post_table.php
+++ database/migrations/2025_04_20_211230_add_soft_delete_to_post_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->softDeletes(); // This will add deleted_at column
+            $table->softDeletes();
         });
     }
 
